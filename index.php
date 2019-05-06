66a25519474de65963b9200f4b0e71b78e41e53f
adcm103 <adcm.103@gmail.com>
2019-05-06 10:51:50 -0500

    modifiquehd

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $row7 = mysqli_fetch_array($result);
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Sobre Pedalcito bonito bien mas bonito mas bonito mas bondddddddddddddddddddddtooo</title>
+	<title>Sobre Pedalcito bonito bien mas bonito mas bonito mas bondddddddddddddddddddddtoddddddddddddddddddddddddddddoo</title>
 	<link rel="stylesheet" type="text/css" href="sobre_pedal/css/estilos.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
